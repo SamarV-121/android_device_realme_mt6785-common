5c1749d02082b78311b57e8cc2cd0b43a9e30f80
SamarV-121 <samarvispute121@gmail.com>
2021-02-13 14:45:41 +0530

    Revert "RMX2001: Add camera aux package list"
    This reverts commit 5c5c9003b21ba836bf65553507b05006319331af.
    
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>
    Change-Id: If147acc2b800490e43675df08edbacafafd6079a

--- properties.mk
+++ properties.mk
@@ -2,11 +2,6 @@
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.surface_flinger.supports_background_blur=1
 
-# Camera
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,com.google.camera \
-    vendor.camera.aux.packageblacklist=org.telegram.messenger,com.microsoft.teams,com.discord
-
 # Dex2oat
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     dalvik.vm.dex2oat64.enabled=true \
