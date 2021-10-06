89143fc5fb89c0c305ce66c5cc1d8613fca5dd8e
SamarV-121 <samarvispute121@pm.me>
2021-10-07 00:02:28 +0530

    mt6785-common: Disable window blur
     * causing screen flicker when using 90hz
    
    Change-Id: Ic5db616c0c0373d08c2005e1002e6c875754617e
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>

--- properties.mk
+++ properties.mk
@@ -1,9 +1,3 @@
-# Blurs
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.surface_flinger.supports_background_blur=1 \
-    ro.sf.blurs_are_expensive=1 \
-    persist.sys.sf.disable_blurs=1
-
 # Bionic
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.bionic.cpu_variant=cortex-a76 \
